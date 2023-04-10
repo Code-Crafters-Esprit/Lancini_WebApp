f18b5f7ab6f3f4ac16f7ca6ef09e809cc885d9b5
Mohamed Ali Naguez <mohamedali.naguez@esprit.tn>
2023-04-10 04:19:29 +0100

    Correction

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,8 +4,6 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
