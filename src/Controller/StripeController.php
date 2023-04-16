caedc015d086d39f7c0c50ea8097efd2455427c0
Mohamed Ali Naguez <mohamedali.naguez@esprit.tn>
2023-04-16 10:04:58 +0100

    Stripe test impelemnted and it works

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -19,26 +19,34 @@ class StripeController extends AbstractController
     }
     #[Route('/pay', name: 'pay')]
     public function pay(Request $request, StripeService $stripeService)
-    {   
-         $amount = $request->request->get('amount');
-        $token = $request->request->get('stripeToken');
-    
-        // create a Stripe Checkout Session and get the session ID
-        $sessionId = $stripeService->createCheckoutSession($amount, 'usd');
-    
-        return $this->render('stripe/pay.html.twig', [
-            'stripePublicKey' => 'pk_test_51Mw8MbH5U7t3MAmZHZDecnuVq8RUQPaVysHyP8OaDAEdQCRVGvPIFMtpIQ4v3ZcFbSSCuSQ8MMpNMSCjH2L3YkBS00jKd2WOxi',
-            'sessionId' => $sessionId,
-            'amount' => $amount,
-            'stripeToken' => $token,
-        ]);
-    }
+{   
+    $amount = $request->request->get('amount');
+    $token = $request->request->get('stripeToken');
+    $successUrl ='https://example.com/success'; // Replace 'success' with your success route name
+    $cancelUrl = 'https://example.com/cancel'; // Replace 'cancel' with your cancel route name
+
+    // create a Stripe Checkout Session and get the session ID
+    $sessionId = $stripeService->createCheckoutSession(100, 'usd', $successUrl, $cancelUrl);
+
+    return $this->render('stripe/pay.html.twig', [
+        'stripePublicKey' => 'pk_test_51Mw8MbH5U7t3MAmZHZDecnuVq8RUQPaVysHyP8OaDAEdQCRVGvPIFMtpIQ4v3ZcFbSSCuSQ8MMpNMSCjH2L3YkBS00jKd2WOxi',
+        'sessionId' => $sessionId,
+        'amount' => 100,
+        'stripeToken' => $token,
+    ]);
+}
 
     #[Route('/success', name: 'success')]
 
     public function success()
     {
         return $this->render('stripe/success.html.twig');
+    }
+    #[Route('/cancel', name: 'cancel')]
+
+    public function cancel()
+    {
+        return $this->render('stripe/cancel.html.twig');
     }
     
 }
--- src/Service/StripeService.php
+++ src/Service/StripeService.php
@@ -3,47 +3,51 @@ namespace App\Service;
 
 use Stripe\Charge;
 use Stripe\Stripe;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class StripeService
 {
     private $stripePublicKey;
     private $stripeSecretKey;
+    private $urlGenerator;
 
-    public function __construct(string $stripePublicKey, string $stripeSecretKey)
+    public function __construct(string $stripePublicKey, string $stripeSecretKey,UrlGeneratorInterface $urlGenerator )
     {
         $this->stripePublicKey = $stripePublicKey;
         $this->stripeSecretKey = $stripeSecretKey;
+        $this->urlGenerator = $urlGenerator;
+
+
+        // Set the Stripe API key
+        Stripe::setApiKey($stripeSecretKey);
     }
 
-    public function charge(int $amount, string $currency, string $token): void
+    public function createCheckoutSession($amount, $currency, $successUrl, $cancelUrl)
     {
-        Stripe::setApiKey($this->stripeSecretKey);
-
-        Charge::create([
-            'amount' => $amount,
-            'currency' => $currency,
-            'source' => $token,
-        ]);
-    }
-    public function createCheckoutSession($amount, $currency)
-{
-    $session = \Stripe\Checkout\Session::create([
-        'payment_method_types' => ['card'],
-        'line_items' => [[
-            'price_data' => [
-                'currency' => $currency,
-                'product_data' => [
-                    'name' => 'My Product',
+        // Convert the amount to cents
+        $amountInCents = floatval($amount) * 100;
+        
+        // Create a new Checkout Session using the Stripe API
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => $currency,
+                    'unit_amount' => $amountInCents, // Stripe requires the amount to be in cents
+                    'product_data' => [
+                        'name' => 'Product Name', // Replace with your product name
+                    ],
                 ],
-                'unit_amount' => $amount,
-            ],
-            'quantity' => 1,
-        ]],
-        'mode' => 'payment',
-       
-    ]);
-
-    return $session->id;
-}
-
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $successUrl,
+            'cancel_url' => $cancelUrl,
+        ]);
+    
+        // Return the ID of the Checkout Session
+        return $session->id;
+    }
+    
 }
