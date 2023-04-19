39a205afd505da7fb4a201f01ad1e5563d9bb1be
Mohamed Ali Naguez <mohamedali.naguez@esprit.tn>
2023-04-19 03:51:02 +0100

    Mail Service added

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Produit;
 use App\Form\Produit1Type;
+use App\Repository\MaillistRepository;
 use App\Repository\ProduitRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,7 +15,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 #[Route('/produit')]
 class ProduitController extends AbstractController
@@ -74,90 +76,56 @@ class ProduitController extends AbstractController
     
     
    #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ProduitRepository $produitRepository): Response
-    {
-        $produit = new Produit();
-        $timezone = $this->getParameter('timezone');
+   public function new(Request $request, ProduitRepository $produitRepository,MaillistRepository $MaillistRepository, MailerInterface $mailer): Response
+{
+    $produit = new Produit();
+    $timezone = $this->getParameter('timezone');
 
-        $produit->setDate(new DateTime('now', new \DateTimeZone($timezone)));
-        $form = $this->createForm(Produit1Type::class, $produit);
-        $form->handleRequest($request);
+    $produit->setDate(new DateTime('now', new \DateTimeZone($timezone)));
+    $form = $this->createForm(Produit1Type::class, $produit);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Handle file upload
-            /** @var UploadedFile $file */
-            $file = $form->get('image')->getData();
+    if ($form->isSubmitted() && $form->isValid()) {
+        // Handle file upload
+        /** @var UploadedFile $file */
+        $file = $form->get('image')->getData();
 
-            if ($file) {
-                $fileName = uniqid() . '.' . $file->guessExtension();
+        if ($file) {
+            $fileName = uniqid() . '.' . $file->guessExtension();
 
-                try {
-                    $file->move(
-                        $this->getParameter('kernel.project_dir') . '/public/images/products',
-                        $fileName
-                    );
+            try {
+                $file->move(
+                    $this->getParameter('kernel.project_dir') . '/public/images/products',
+                    $fileName
+                );
 
-                    $produit->setImage($fileName);
-                } catch (FileException $e) {
-                    // Handle exception
-                }
+                $produit->setImage($fileName);
+            } catch (FileException $e) {
+                // Handle exception
+            }
+            $Maillist = $MaillistRepository->findAll();
+            foreach ($Maillist as $subscriber) {
+                $email = (new Email())
+                    ->from('lancinimarket@gmail.com')
+                    ->to($subscriber->getEmail())
+                    ->subject('New Product Alert!')
+                    ->html('<p>Check out our new product!</p>');
+
+                $mailer->send($email);
             }
 
             $produitRepository->save($produit, true);
 
             return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->renderForm('produit/edit.html.twig', [
-            'produit' => $produit,
-            'form' => $form,
-        ]);
     }
 
-    #[Route('/{idproduit}', name: 'app_produit_show', methods: ['GET'])]
-    public function show(Produit $produit): Response
-    {
-        return $this->render('produit/show.html.twig', [
-            'produit' => $produit,
-        ]);
-    }
+    return $this->renderForm('produit/edit.html.twig', [
+        'produit' => $produit,
+        'form' => $form,
+    ]);
+}
 
-    #[Route('/{idproduit}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Produit $produit, ProduitRepository $produitRepository, UploaderHelper $uploaderHelper): Response
-    {
-        $form = $this->createForm(Produit1Type::class, $produit);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Handle file upload
-            /** @var UploadedFile $file */
-            $file = $form->get('image')->getData();
-
-            if ($file) {
-                $fileName = uniqid() . '.' . $file->guessExtension();
-
-                try {
-                    $file->move(
-                        $this->getParameter('kernel.project_dir') . '/public/images/products',
-                        $fileName
-                    );
-
-                    $produit->setImage($fileName);
-                } catch (FileException $e) {
-                    // Handle exception
-                }
-            }
-
-            $produitRepository->save($produit, true);
-
-            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('produit/edit.html.twig', [
-            'produit' => $produit,
-            'form' => $form,
-        ]);
-    }
 
 #[Route('/{idproduit}', name: 'app_produit_delete', methods: ['POST'])]
 public function delete(Request $request, Produit $produit, ProduitRepository $produitRepository): Response
