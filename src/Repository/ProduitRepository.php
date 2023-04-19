88e35f9046251e286185f30bcc40433ad83d0870
Mohamed Ali Naguez <mohamedali.naguez@esprit.tn>
2023-04-19 09:43:42 +0100

    correction filtres

--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -38,87 +38,90 @@ class ProduitRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-    public function findByFilters($searchTerm = null, array $categoryFilters = [], array $priceFilters = [], array $orderBy = null, $limit = null, $offset = null): array
-    {
-        $queryBuilder = $this->createQueryBuilder('p');
+   
+ public function findByFilters($searchTerm = null, array $categoryFilters = [], array $priceFilters = [], array $orderBy = null, $limit = null, $offset = null): array
+ {
+     $queryBuilder = $this->createQueryBuilder('p');
 
-        // Apply search term filter
-        if (!empty($searchTerm)) {
-            $queryBuilder
-                ->andWhere('p.nom LIKE :searchTerm')
-                ->setParameter('searchTerm', '%' . $searchTerm . '%');
-        }
+     // Apply search term filter
+     if (!empty($searchTerm)) {
+         $queryBuilder
+             ->andWhere('p.nom LIKE :searchTerm')
+             ->setParameter('searchTerm', '%' . $searchTerm . '%');
+     }
 
-        // Apply category filters
-        if (!empty($categoryFilters)) {
-            $queryBuilder
-                ->andWhere('p.categorie IN (:categories)')
-                ->setParameter('categories', $categoryFilters);
-        }
+     // Apply category filters
+     if (!empty($categoryFilters)) {
+         $queryBuilder
+             ->andWhere('p.categorie IN (:categories)')
+             ->setParameter('categories', $categoryFilters);
+     }
 
-        // Apply price filters
-        if (!empty($priceFilters)) {
-            $priceRangeFilters = [];
-            foreach ($priceFilters as $priceFilter) {
-                switch ($priceFilter) {
-                    case 'price1':
-                        $priceRangeFilters[] = [
-                            'min' => 0,
-                            'max' => 5,
-                        ];
-                        break;
-                    case 'price2':
-                        $priceRangeFilters[] = [
-                            'min' => 5,
-                            'max' => 15,
-                        ];
-                        break;
-                    case 'price3':
-                        $priceRangeFilters[] = [
-                            'min' => 15,
-                            'max' => null,
-                        ];
-                        break;
-                }
-            }
-            if (!empty($priceRangeFilters)) {
-                $orX = $queryBuilder->expr()->orX();
-                foreach ($priceRangeFilters as $priceRangeFilter) {
-                    if ($priceRangeFilter['max'] !== null) {
-                        $orX->add(
-                            $queryBuilder->expr()->andX(
-                                $queryBuilder->expr()->gte('p.prix', $priceRangeFilter['min']),
-                                $queryBuilder->expr()->lte('p.prix', $priceRangeFilter['max'])
-                            )
-                        );
-                    } else {
-                        $orX->add(
-                            $queryBuilder->expr()->gte('p.prix', $priceRangeFilter['min'])
-                        );
-                    }
-                }
-                $queryBuilder->andWhere($orX);
-            }
-        }
+     // Apply price filters
+     if (!empty($priceFilters)) {
+         $priceRangeFilters = [];
+         foreach ($priceFilters as $priceFilter) {
+             switch ($priceFilter) {
+                 case 'price1':
+                     $priceRangeFilters[] = [
+                         'min' => 0,
+                         'max' => 5,
+                     ];
+                     break;
+                 case 'price2':
+                     $priceRangeFilters[] = [
+                         'min' => 5,
+                         'max' => 15,
+                     ];
+                     break;
+                 case 'price3':
+                     $priceRangeFilters[] = [
+                         'min' => 15,
+                         'max' => null,
+                     ];
+                     break;
+             }
+         }
+         if (!empty($priceRangeFilters)) {
+             $orX = $queryBuilder->expr()->orX();
+             foreach ($priceRangeFilters as $priceRangeFilter) {
+                 if ($priceRangeFilter['max'] !== null) {
+                     $orX->add(
+                         $queryBuilder->expr()->andX(
+                             $queryBuilder->expr()->gte('p.prix', $priceRangeFilter['min']),
+                             $queryBuilder->expr()->lte('p.prix', $priceRangeFilter['max'])
+                         )
+                     );
+                 } else {
+                     $orX->add(
+                         $queryBuilder->expr()->gte('p.prix', $priceRangeFilter['min'])
+                     );
+                 }
+             }
+             $queryBuilder->andWhere($orX);
+         }
+     }
 
-        // Apply order by clause
-        if ($orderBy !== null) {
-            foreach ($orderBy as $field => $direction) {
-                $queryBuilder->orderBy('p.' . $field, $direction);
-            }
-        }
+     // Apply order by clause
+     if ($orderBy !== null) {
+         foreach ($orderBy as $field => $direction) {
+             $queryBuilder->orderBy('p.' . $field, $direction);
+         }
+     }
 
-        // Apply limit and offset clauses
-        if ($limit !== null) {
-            $queryBuilder->setMaxResults($limit);
-        }
+     // Apply limit and offset clauses
+     if ($limit !== null) {
+         $queryBuilder->setMaxResults($limit);
+     }
 
-        if ($offset !== null) {
-            $queryBuilder->setFirstResult($offset);
-        }
+     if ($offset !== null) {
+         $queryBuilder->setFirstResult($offset);
+     }
 
-        return $queryBuilder->getQuery()->getResult();
-    }
+     return $queryBuilder->getQuery()->getResult();
+ }
+
+
     public function findProductsByVendeur()
     {
         $qb = $this->createQueryBuilder('p')
