878ffaeed616a233e5cfa3f0033bcd1d7d183cd1
Mohamed Ali Naguez <mohamedali.naguez@esprit.tn>
2023-04-26 05:21:41 +0100

    last touches

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Locale\LocaleResolver;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,18 +12,24 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LanguageController extends AbstractController
 {
-    #[Route("/language/{locale}", name:"language_switcher")]
-    public function switchLocale(Request $request, SessionInterface $session, $locale): Response
+    private $session;
+    private $localeResolver;
+
+    public function __construct(SessionInterface $session, LocaleResolver $localeResolver)
     {
-        // Set the new locale in the session
-        $session->set('locale', $locale);
-
-        // Set the new default locale using a custom service
-        $localeResolver = $this->get('App\Locale\LocaleResolver');
-        $localeResolver->setDefaultLocale($locale);
-
-        // Redirect the user back to the previous page
-        $referer = $request->headers->get('referer');
-        return new RedirectResponse($referer);
+        $this->session = $session;
+        $this->localeResolver = $localeResolver;
     }
+
+    #[Route("/language/{locale}", name:"language_switcher")]
+    public function switchLocale(Request $request, $locale): Response
+    {
+       // Set the new locale in the session
+       $this->session->set('locale', $locale);
+       $this->localeResolver->setDefaultLocale($locale);
+
+       // Redirect the user back to the previous page
+       $referer = $request->headers->get('referer');
+       return new RedirectResponse($referer);
+   }
 }
